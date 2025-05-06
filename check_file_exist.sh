b3954d0992435b89a5ba90aa60c74c218f0a8663
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-05-06 13:06:17 +0100

    Delete check_file_exist.sh

--- check_file_exist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ce code retourne réponse à la question : est-ce que le fichier " hello_world.sh" existe
-
-[ -f hello_world.sh ] && echo "le fichier existe" || echo "le fichier n'existe pas"
